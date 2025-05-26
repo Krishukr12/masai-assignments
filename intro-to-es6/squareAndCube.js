8c2eb46b31ae12cfcc538a0315514e4c959d091a
krishan kumar safi <krishankumarsafi7@gmail.com>
2025-05-26 17:57:56 +0530

    add-squareandcube-fn

---
+++ intro-to-es6/squareAndCube.js
@@ -0,0 +1,7 @@
+// Write an arrow function that takes a number as input and returns both its square and cube in an object, 
+// using implicit return. The function should be as concise as possible.
+
+// Example Output:
+// { square: 25, cube: 125 }
+
+const squareAndCube=(number)=>  ({square:number*number,cube:number*number*number});
