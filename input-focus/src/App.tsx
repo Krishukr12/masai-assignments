e13725ac5e140462201536abeb0f65f1d289d8a2
krishan kumar safi <krishankumarsafi7@gmail.com>
2025-06-08 22:11:20 +0530

    fix-bug

--- input-focus/src/App.tsx
+++ input-focus/src/App.tsx
@@ -1,3 +1,31 @@
+import { useRef, useState } from "react";
+
 export const App = () => {
-  return <main>Main Section</main>;
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  const [isFocused, setIsFocused] = useState<boolean>(false);
+
+  const handleFocusInput = () => {
+    inputRef.current?.focus();
+    if (inputRef.current) {
+      inputRef.current.style.backgroundColor = "blue";
+      inputRef.current.style.color = "white";
+    }
+    setIsFocused(true);
+  };
+
+  return (
+    <main className="min-h-screen border border-red-300 flex justify-center items-center">
+      <section className="flex flex-col gap-4">
+        <input
+          className="shadow p-5 outline-blue-600"
+          ref={inputRef}
+          placeholder="Enter name... "
+        />
+        {isFocused ? <h3>Focused</h3> : null}
+        <button className="shadow p-2" onClick={handleFocusInput}>
+          Focus Input
+        </button>
+      </section>
+    </main>
+  );
 };
